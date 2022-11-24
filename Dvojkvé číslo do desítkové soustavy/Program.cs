5a7b4c4119ebd767d597c5a30beadda416f75067
drozboril <rozborildavid77@gmail.com>
2022-11-24 19:23:42 +0100

    Uživatel zadá binární číslo, program ho zpracuje do desítkové podoby

---
+++ Dvojkvé číslo do desítkové soustavy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte dvojkové číslo k převodu do desítkové soustavy");
+            int bin = int.Parse(Console.ReadLine());
+            int bin1 = bin;
+            int i, j, k, l;
+            i = bin / 1000; j = bin / 100 % 10; k = bin / 10 % 10; l = bin % 10;
+            int dec = 0;
+            int a = 1;
+            if (i > 1 || j > 1 || k > 1 || l > 1)
+            {
+                Console.WriteLine("Chyba zadání");
+            }
+            else
+            {
+                while (bin > 0)
+                {
+                    dec += (a * (bin % 2));
+                    bin = bin / 10;
+                    a = a << 1;
+                }
+                Console.WriteLine("Zadané číslo {0} je v desítkové soustavě {1}", bin1, dec);
+            }
+            Console.ReadLine();
+        }
+    }
+}
